ae7e2d2c2a4424ae5e9f429d7a447cb6cfc17d94
josephbill <josephbill00@gmail.com>
2022-02-06 20:59:39 +0300

    COmpleted Assignments

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const mainDiv = document.getElementById("main");
+//DOM removing an element
+mainDiv.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.innerHTML = "Joseph is the champion!";
